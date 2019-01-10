616b3dc64f3de0cafe17f5ffc2f6b94c1594cb9c
liuyib <1656081615@qq.com>
2019-01-10 11:34:48 +0800

    node API url学习笔记1

---
+++ node/imooc/createModule/url/test.js
@@ -0,0 +1,1 @@
+"protocol:'https',hostname:'example.com',pathname:'/some/path',query:{page:1,format:'json'}"
---
+++ node/imooc/createModule/url/url.js
@@ -0,0 +1,5 @@
+var url = require('url');
+
+var urlParse = url.parse('https://www.imooc.com/courses', false);
+
+console.log(urlParse);
