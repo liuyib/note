92cc2915bda9edb1ca8ae1a5f35f00e99908f1c9
liuyib <1656081615@qq.com>
2019-01-17 15:45:02 +0800

    grasp code from imooc by node http

---
+++ Node/imooc/http/http_crawler.js
@@ -0,0 +1,28 @@
+// 爬取网页源码
+
+var http = require('http');
+
+var url = 'http://www.imooc.com/learn/348/';
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('http crawler test');
+
+  res.end();
+}).listen(8888);
+
+http.get(url, function (res) {
+  var html = '';
+
+  // 监听data事件
+  res.on('data', function (data) {
+    html += data;
+  })
+
+  // 请求结束，将所有爬取到的html打印出来
+  res.on('end', function () {
+    console.log(html);
+  })
+}).on('error', function () {
+  console.log('爬取失败');
+});
